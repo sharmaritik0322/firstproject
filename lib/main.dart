e5aec8876983929907cc110525122fa545b8ec01
Ritik Sharma <ritiksharma2215@gmail.com>
2023-03-06 11:17:51 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Center(
+      child: Text(
+    'Hello Flutter',
+    textDirection: TextDirection.ltr,
+    style: TextStyle(fontSize: 50.0), ,
+  )));
+  DecoratedBox: color:Color;
+}
